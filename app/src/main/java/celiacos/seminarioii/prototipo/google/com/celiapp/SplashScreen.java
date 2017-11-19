30542007d6814b3d96f4c9d6dc9dffaeca69916d
thelowend <thelowend6@gmail.com>
2017-11-18 21:46:47 -0300

    Base App

---
+++ app/src/main/java/celiacos/seminarioii/prototipo/google/com/celiapp/SplashScreen.java
@@ -0,0 +1,54 @@
+package celiacos.seminarioii.prototipo.google.com.celiapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+public class SplashScreen extends AppCompatActivity {
+
+    ProgressBar barrita;
+    int progreso = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_splash_screen);
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+
+                    barrita = (ProgressBar) findViewById(R.id.progressBar);
+                    barrita.setVisibility(View.VISIBLE);
+                    setProgresoBarrita(progreso);
+
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+
+    public void setProgresoBarrita(final int progreso) {
+        barrita.setProgress(progreso);
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                setProgresoBarrita(progreso + 15);
+            }
+        });
+        thread.start();
+    }
+}
