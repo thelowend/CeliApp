240616f07229a29188430817e13246007911f4be
thelowend <thelowend6@gmail.com>
2017-11-19 18:11:57 -0300

    Ongoing updates

--- app/src/main/java/celiacos/seminarioii/prototipo/google/com/celiapp/MainActivity.java
+++ app/src/main/java/celiacos/seminarioii/prototipo/google/com/celiapp/MainActivity.java
@@ -1,16 +1,12 @@
 package celiacos.seminarioii.prototipo.google.com.celiapp;
 
-import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Criteria;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.view.View;
@@ -24,20 +20,26 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.RelativeLayout;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.maps.android.ui.IconGenerator;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
 
-    private GoogleMap mMap;
     private FirebaseDatabase db;
+
+    private FirebaseAuth mAuth;
+
+    private GoogleMap mMap;
     private LocationManager locationManager;
     private Location usrLocation;
     private View mapView;
@@ -48,23 +50,32 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //
         setSupportActionBar(toolbar);
+
+        //getSupportActionBar().setHomeAsUpIndicator(R.mipmap.ic_launcher);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+        toggle.setHomeAsUpIndicator(R.mipmap.ic_launcher);
+
+        toolbar.setNavigationIcon(R.mipmap.ic_launcher);
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        //Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
         //Firebase Database
         db = FirebaseDatabase.getInstance();
 
-        DatabaseReference myRef = db.getReference("Locations");
+        DatabaseReference dbRef = db.getReference();
+        DatabaseReference locationsReference = db.getReference("Locations");
 
-        myRef.setValue("Hello, World!");
 
 
         //Map
@@ -74,8 +85,68 @@ public class MainActivity extends AppCompatActivity
         mapFragment.getMapAsync(this);
     }
 
-    private void LoadMarkers () {
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        //updateUI(currentUser);
+    }
+
+    private void loadMarkers () {
+
+        //load data from DB
+        //DatabaseReference myRef = db.getReference("Locations");
+
+        //myRef
+
         //mMap
+        IconGenerator icnGeneratorRG = new IconGenerator(this);
+        IconGenerator icnGeneratorRP = new IconGenerator(this);
+        IconGenerator icnGeneratorGG = new IconGenerator(this);
+
+
+
+        final BitmapDrawable restaurantGreen = (BitmapDrawable) this.getResources()
+                .getDrawable(R.mipmap.ic_restaurant_green);
+
+        final BitmapDrawable restaurantPink = (BitmapDrawable) this.getResources()
+                .getDrawable(R.mipmap.ic_restaurant_pink);
+
+        final BitmapDrawable glutenFreeGreen = (BitmapDrawable) this.getResources()
+                .getDrawable(R.mipmap.ic_gluten_free);
+
+        icnGeneratorRG.setBackground(restaurantGreen);
+        icnGeneratorRP.setBackground(restaurantPink);
+        icnGeneratorGG.setBackground(glutenFreeGreen);
+
+        Bitmap restaurantGreenBitmap = icnGeneratorRG.makeIcon();
+        Bitmap restaurantPinkBitmap = icnGeneratorRP.makeIcon();
+        Bitmap glutenFreeGreenBitmap = icnGeneratorGG.makeIcon();
+
+        //UADE
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-34.616877, -58.381783))
+                .icon(BitmapDescriptorFactory.fromBitmap(glutenFreeGreenBitmap)).anchor(0.5f, 0.6f));
+
+        //CID CAMPEADOR
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-34.607568, -58.445721))
+                .icon(BitmapDescriptorFactory.fromBitmap(restaurantPinkBitmap)).anchor(0.5f, 0.6f));
+
+        //Random cerca de UADE
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-34.614825, -58.381750))
+                .icon(BitmapDescriptorFactory.fromBitmap(restaurantGreenBitmap)).anchor(0.5f, 0.6f));
+
+        //Random cerca de UADE 2
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-34.617774, -58.385140))
+                .icon(BitmapDescriptorFactory.fromBitmap(restaurantPinkBitmap)).anchor(0.5f, 0.6f));
+
+        //Random cerca de UADE 3
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-34.615011, -58.376750))
+                .icon(BitmapDescriptorFactory.fromBitmap(restaurantGreenBitmap)).anchor(0.5f, 0.6f));
+
+
+        //myPlaceItemMarkers.add();
     }
 
     @Override
@@ -94,9 +165,11 @@ public class MainActivity extends AppCompatActivity
             layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
             layoutParams.setMargins(0, 0, 30, 30);
 
+            locationButton.performClick();
+
         }
 
-        //loadMarkers();
+        loadMarkers();
 
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
